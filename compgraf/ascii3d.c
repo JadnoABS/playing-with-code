69fb17bf1f5ed923624499859051b5ba16b4067e
JadnoABS <vlogggamers@google.com>
2021-10-07 17:52:20 -0300

    Cubinho girante

--- compgraf/ascii3d.c
+++ compgraf/ascii3d.c
@@ -9,12 +9,10 @@
 #endif
 
 #define PI 3.14159265
-#define RESOLUTION 30
+#define RESOLUTION 45
 
 
-char depth[] = {'#', '@', 'm', 'o', '.'};
-
-
+char depth[] = {'@', '0', 'O', 'o', '.'};
 
 /*char setDepth(int zindex) {*/
   /*if(zindex < 25) return depth[0];*/
@@ -25,17 +23,17 @@ char depth[] = {'#', '@', 'm', 'o', '.'};
 /*}*/
 
 char setDepthChar(int zindex) {
-  if(zindex < 8) return depth[0];
-  if(zindex < 16) return depth[1];
-  if(zindex < 24) return depth[2];
+  if(zindex < 20) return depth[0];
+  if(zindex < 25) return depth[1];
+  if(zindex < 30) return depth[2];
   if(zindex < 100) return depth[3];
   return depth[4];
 }
 
 int main() {
 
-  int visionPoint[] = {12, 12, 0};
-  int depthCoeficient = 5;
+  int visionPoint[] = {22, 22, 0};
+  int depthCoeficient = 15;
 
   /*int cubo[8][3] = {*/
     /*{10, 10, 0},*/
@@ -59,7 +57,7 @@ int main() {
     /*{55, 55, 50},*/
   /*};*/
 
-  int cubo[8][3] = {
+  float cubo[8][3] = {
     {2, 2, 20},
     {22, 2, 20},
     {2, 22, 20},
@@ -70,25 +68,30 @@ int main() {
     {22, 22, 40},
   };
 
-  int halfSide = cubo[1][0] - cubo[0][0];
+  for(int i = 0; i < 8; i++){
+    cubo[i][0] += 10;
+    cubo[i][1] += 10;
+  }
+
+  int halfSide = (cubo[1][0] - cubo[0][0])/2;
   int center[] = {
     cubo[0][0] + halfSide,
     cubo[0][1] + halfSide,
     cubo[0][2] + halfSide
   };
 
-  int maxX = center[0] + halfSide;
-  int maxY = center[1] + halfSide;
-  int maxZ = center[2] + halfSide;
+  int maxX = center[0] + (halfSide * pow(2, 1/2));
+  int maxY = center[1] + (halfSide * pow(2, 1/2));
+  int maxZ = center[2] + (halfSide * pow(2, 1/2));
 
   char display[RESOLUTION][RESOLUTION];
 
 
   int angle = 1;
-  float theta = PI / 180;
-  while(angle <= 90) {
+  float theta = PI/180;
+  while(angle <= 180) {
     usleep(100000);
-    system("clear");
+    /*system("clear");*/
 
     for(int i = 0; i < RESOLUTION; i++){
       for(int j = 0; j < RESOLUTION; j++){
@@ -99,7 +102,6 @@ int main() {
     for(int i = 0; i < 8; i++) {
 
       char ch = setDepthChar(cubo[i][2]);
-
 
       int multX = visionPoint[0] < cubo[i][0] ? -1 : 1;
       int multY = visionPoint[1] < cubo[i][1] ? -1 : 1;
@@ -114,11 +116,22 @@ int main() {
       }
 
       /* Rotacao do cubo em torno de Y */
-      int sentidoZ = cubo[i][0] < center[0] ? 1 : -1;
-      int sentidoX = cubo[i][2] < center[0] ? -1 : 1;
 
-      cubo[i][0] += cos(theta) * sentidoX;
-      cubo[i][2] += sin(theta) * sentidoZ;
+      /*if(i == 4 || i == 6) {*/
+        /*cubo[i][0] += 0.9;*/
+        /*cubo[i][2] += 0.1;*/
+      /*}*/
+
+      int sentidoZ = cubo[i][0] <= center[0] ? 1 : -1;
+      int sentidoX = cubo[i][2] <= center[2] ? -1 : 1;
+
+      if(sentidoX == sentidoZ) {
+        cubo[i][0] += cos(theta) * sentidoX;
+        cubo[i][2] += cos(theta) * sentidoZ;
+      } else {
+        cubo[i][0] += cos(theta) * sentidoX;
+        cubo[i][2] += cos(theta) * sentidoZ;
+      }
     }
 
     for(int i = 0; i < RESOLUTION; i++){
